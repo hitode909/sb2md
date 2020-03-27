ab4dec469cb31b37a8fdd0a1765efc199b861690
Jun Kato <i@junkato.jp>
2020-03-27 19:06:58 +0900

    Add failing test case

--- test/sb2md.js
+++ test/sb2md.js
@@ -17,6 +17,10 @@ test('link', t => {
   t.is(sb2md('[日本語]'), '[日本語](./%E6%97%A5%E6%9C%AC%E8%AA%9E.md)');
 });
 
+test('numerical link', t => {
+  t.is(sb2md('[0]'), "[0](./0.md)");
+});
+
 test('hashtag', t => {
   t.is(sb2md('#日本語'), '[#日本語](./%E6%97%A5%E6%9C%AC%E8%AA%9E.md)');
 });
